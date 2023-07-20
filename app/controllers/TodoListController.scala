ab9984f9af5b9445b9a87596b33090c04b6f0d5a
jnieblas <53878113+jnieblas@users.noreply.github.com>
2023-07-19 18:08:33 -0600

    Adding POST & PUT endpoints for todoList

--- app/controllers/TodoListController.scala
+++ app/controllers/TodoListController.scala
@@ -4,32 +4,67 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import scala.collection.mutable
-import models.TodoListItem
+import models._
 import play.api.libs.json._
 
 @Singleton
-class TodoListController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  
-    // implicit to avoid passing to Json.toJson() all the time
-    implicit val todoListJson: OFormat[TodoListItem] = Json.format[TodoListItem]
+class TodoListController @Inject() (
+    val controllerComponents: ControllerComponents
+) extends BaseController {
 
-    private val todoList = new mutable.ListBuffer[TodoListItem]()
-    todoList += TodoListItem(1, "test", true)
-    todoList += TodoListItem(2, "some other value", false)
+  // implicit to avoid passing to Json.toJson() all the time
+  implicit val todoListJson: OFormat[TodoListItem] = Json.format[TodoListItem]
+  implicit val newTodoListJson: OFormat[NewTodoListItem] =
+    Json.format[NewTodoListItem]
 
-    def getAll(): Action[AnyContent] = Action {
-        if (todoList.isEmpty) {
-            NoContent
-        } else {
-            Ok(Json.toJson(todoList))
-        }
+  private val todoList = new mutable.ListBuffer[TodoListItem]()
+  todoList += TodoListItem(1, "test", false)
+  todoList += TodoListItem(2, "some other value", false)
+
+  def getAll(): Action[AnyContent] = Action {
+    if (todoList.isEmpty) {
+      NoContent
+    } else {
+      Ok(Json.toJson(todoList))
     }
+  }
 
-    def getById(itemId: Long): Action[AnyContent] = Action {
-        val foundItem = todoList.find(_.id == itemId)
-        foundItem match {
-            case Some(item) => Ok(Json.toJson(item))
-            case None => NotFound
-        }
+  def getById(itemId: Long): Action[AnyContent] = Action {
+    val foundItem = todoList.find(_.id == itemId)
+    foundItem match {
+      case Some(item) => Ok(Json.toJson(item))
+      case None       => NotFound
     }
+  }
+
+  def addNewItem(): Action[AnyContent] = Action { implicit request =>
+    val jsonObject = request.body.asJson
+    val todoListItem: Option[NewTodoListItem] =
+      jsonObject.flatMap(
+        Json.fromJson[NewTodoListItem](_).asOpt
+      )
+
+    todoListItem match {
+      case Some(newItem) =>
+        val nextId = todoList.map(_.id).max + 1
+        val toBeAdded = TodoListItem(nextId, newItem.description, false)
+        todoList += toBeAdded
+        Created(Json.toJson(toBeAdded))
+      case None => BadRequest
+    }
+  }
+
+  def markAsDone(itemId: Long): Action[AnyContent] = Action {
+    val foundItem = todoList.find(_.id == itemId)
+
+
+    foundItem match {
+      case Some(item: TodoListItem) =>
+        val updatedItem = item.copy(isItDone = true)
+        todoList -= item
+        todoList += updatedItem
+        Created(Json.toJson(updatedItem))
+      case None => NotFound
+    }
+  }
 }
