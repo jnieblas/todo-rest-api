d2c62a4775cefea80b23aea817d13cc82d40d96d
jnieblas <53878113+jnieblas@users.noreply.github.com>
2023-07-19 23:44:57 -0600

    Adding tests for addNewItem

--- test/controllers/TodoListControllerSpec.scala
+++ test/controllers/TodoListControllerSpec.scala
@@ -4,6 +4,9 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.http.HeaderNames
+import play.api.libs.json.Json
+import akka.actor.ActorSystem
 
 // NEED TO FIX THESE
 /** Add your spec here. You can mock out a whole application including requests,
@@ -16,6 +19,8 @@ class TodoListControllerSpec
     extends PlaySpec
     with GuiceOneAppPerTest
     with Injecting {
+
+  implicit val actorSystem: ActorSystem = ActorSystem("test")
 
   "TodoListController GET all" should {
     "return a list of todo items" in {
@@ -44,7 +49,7 @@ class TodoListControllerSpec
   "TodoListController GET by id" should {
     "return a single todo item" in {
       val controller = new TodoListController(stubControllerComponents())
-      val response = controller.getById(1).apply(FakeRequest(GET, "/2"))
+      val response = controller.getById(1).apply(FakeRequest(GET, "/"))
 
       status(response) mustBe OK
       contentType(response) mustBe Some("application/json")
@@ -61,6 +66,36 @@ class TodoListControllerSpec
       contentType(response) mustBe Some("application/json")
       contentAsString(response) must include(
         "{\"id\":1,\"description\":\"test\",\"isItDone\":false}"
+      )
+    }
+  }
+
+  "TodoListController POST item" should {
+    "create a todo item" in {
+      val body = Json.obj("description" -> "my new todo")
+      val request = FakeRequest(POST, "/")
+        .withJsonBody(body)
+      val controller = new TodoListController(stubControllerComponents())
+      val response = controller
+        .addNewItem()(request)
+
+      status(response) mustBe CREATED
+      contentType(response) mustBe Some("application/json")
+      contentAsString(response) must include(
+        "{\"id\":3,\"description\":\"my new todo\",\"isItDone\":false}"
+      )
+    }
+
+    "be routable" in {
+      val body = Json.obj("description" -> "my new todo")
+      val request = FakeRequest(POST, "/todo")
+        .withJsonBody(body)
+      val response = route(app, request).get
+
+      status(response) mustBe CREATED
+      contentType(response) mustBe Some("application/json")
+      contentAsString(response) must include(
+        "{\"id\":3,\"description\":\"my new todo\",\"isItDone\":false}"
       )
     }
   }
